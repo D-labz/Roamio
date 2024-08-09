b0e7553d53c3482bf8fae5bc9f615f347420b626
Prashidhika Neupane <mununeupane8@gmail.com>
2024-08-09 17:05:31 +0100

    haha

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a href="https://github.com/D-labz/Roamio" target="_blank">
+        Click here to go to the repo!
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
